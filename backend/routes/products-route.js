a873acb0766a9b133c4a08aaab7ad1415b39c2d9
Aly Zidan Aly <aaly@sure.com.sa>
2021-11-01 13:45:39 +0200

    correcting rest.http request and adding more client requests, adding edit product action

--- backend/routes/products-route.js
+++ backend/routes/products-route.js
@@ -2,11 +2,87 @@ const express = require('express')
 const router = express.Router()
 const { Product } = require('../models/products')
 const { Category } = require('../models/categories')
+const mongoose = require('mongoose')
 
 router.get(`/`, async (req, res) => {
     const productsList = await Product.find()
+    // const productsList = await Product.find().select('name -_id')
     if (!productsList) return res.status(500).json({ success: false })
     res.send(productsList)
+})
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleteProduct = await Product.findByIdAndRemove(id)
+        return res.status(200).json({
+            success: true,
+            message: 'the product was successfully deleted',
+        })
+    } catch (e) {
+        return res.status(404).json({
+            error: e,
+            message: 'connot delete the requested product',
+            success: false,
+        })
+    }
+})
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id).populate('category')
+    if (!product)
+        return res.status(500).json({
+            message: 'could not find the requested product',
+            success: false,
+        })
+    return res.status(200).send(product)
+})
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const {
+        name,
+        description,
+        richDescription,
+        brand,
+        price,
+        category,
+        numReviews,
+        isFeatured,
+        image,
+        countInStock,
+        rating,
+    } = req.body
+    if (!mongoose.isValidObjectId(id)) {
+        return req.status(400).send('Invalid Product id')
+    }
+    const categoryCheck = await Category.findById(category)
+    if (!categoryCheck) return res.status(400).send('Invalid Category id')
+    const product = await Product.findByIdAndUpdate(
+        id,
+        {
+            name,
+            description,
+            richDescription,
+            brand,
+            price,
+            category,
+            numReviews,
+            isFeatured,
+            image,
+            countInStock,
+            rating,
+        },
+        {
+            new: true,
+        }
+    )
+    if (!product) {
+        return res.status(400).json({
+            message: 'operation could not be completed',
+            error: product,
+        })
+    }
+    return res.status(200).send(product)
 })
 
 router.post(`/`, async (req, res) => {
